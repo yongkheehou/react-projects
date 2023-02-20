c82293295637119c0e5bd800c864085bac06afeb
yongkheehou <yongkheehou@gmail.com>
2023-02-20 23:49:31 +0800

    adding a form

--- food_order_app/src/App.js
+++ food_order_app/src/App.js
@@ -1,6 +1,6 @@
-import { Fragment } from 'react';
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
