f6cfa701eb6ebff700df19ce98dd6c5bf939b303
yongkheehou <yongkheehou@gmail.com>
2023-02-27 21:30:59 +0800

    testing child component re-evaluation

---
+++ react_behind_the_scenes_and_optimization/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/demo/Demo_output";
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("1");
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+    </div>
+  );
+}
+
+export default App;
