62dc50708dc661d36557f88da46e5ac22d99d7b1
yongkheehou <yongkheehou@gmail.com>
2023-03-08 22:08:43 +0800

    using useEffect

--- connecting_to_db/src/App.js
+++ connecting_to_db/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -7,6 +7,7 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
   // const dummyMovies = [
   //   {
   //     id: 1,
@@ -22,7 +23,7 @@ function App() {
   //   },
   // ];
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -48,7 +49,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies</p>;
 
