d61355aded182b1eed44311d17a0505fabafb140
yongkheehou <yongkheehou@gmail.com>
2023-03-25 21:04:52 +0800

    create redux store for react

---
+++ redux/src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from "redux";
+
+const counterReduer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReduer);
+
+export default store;
