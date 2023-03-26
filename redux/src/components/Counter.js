a3b513b0a24f8bb6c9a3e00e3fe7cc0dd19328dd
yongkheehou <yongkheehou@gmail.com>
2023-03-26 09:00:35 +0800

    using redux toolkit to pass payloads and update states

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 // import { Component, connect } from "react"; // use for class based component code
 
 import classes from "./Counter.module.css";
+import { counterActions } from "../store/index";
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -10,19 +11,19 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -13,9 +13,9 @@ const counterSlice = createSlice({
       state.counter--;
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount;
+      state.counter = state.counter + action.payload;
     },
-    toggleCOunter(state) {
+    toggleCounter(state) {
       state.showCounter = !state.showCounter;
     },
   },
@@ -25,5 +25,7 @@ const store = configureStore({
   reducer: counterSlice.reducer,
   // { counter: counterSlice.reducer },
 });
+
+export const counterActions = counterSlice.actions;
 
 export default store;
