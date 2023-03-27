28c793adc57a98e8bd2a94dd8afe2eb76b297496
yongkheehou <yongkheehou@gmail.com>
2023-03-27 09:05:20 +0800

    adding another slice in redux store for auth

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -6,9 +6,9 @@ import { counterActions } from "../store/index";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
+  const counter = useSelector((state) => state.counter.counter);
 
-  const show = useSelector((state) => state.showCounter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
