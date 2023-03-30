cc14fe358e4a6ed85b3e393519a7d4158a489e28
yongkheehou <yongkheehou@gmail.com>
2023-03-30 10:32:45 +0800

    remove dispatch logic from component by using Action Creator Thunk

--- advanced_redux/src/store/cart-slice.js
+++ advanced_redux/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -40,6 +41,51 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "sending",
+        message: "sending cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-beginner-project-c4a78-default-rtdb.firebaseio.com//cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "sent",
+          message: "sent cart data",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "error",
+          message: "error occured",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
