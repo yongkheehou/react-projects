df4a3146291b2df79eabf56c3e57cb1df93c87b7
yongkheehou <yongkheehou@gmail.com>
2023-03-31 08:59:08 +0800

    getting started with fetching data from firebase with redux

---
+++ advanced_redux/src/store/cart-actions.js
@@ -0,0 +1,78 @@
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-beginner-project-c4a78-default-rtdb.firebaseio.com//cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "error",
+          message: "fetching cart data failed occured",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "sending",
+        message: "sending cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-beginner-project-c4a78-default-rtdb.firebaseio.com//cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "sent",
+          message: "sent cart data",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "error",
+          message: "error occured",
+        })
+      );
+    }
+  };
+};
