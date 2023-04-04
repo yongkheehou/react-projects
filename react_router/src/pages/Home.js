b0ec92748cc10b54ab9950c210313ddb5cb2ce8c
yongkheehou <yongkheehou@gmail.com>
2023-04-04 10:23:16 +0800

    navigating between pages with links

--- react_router/src/pages/Home.js
+++ react_router/src/pages/Home.js
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <h1>Homepage</h1>;
+  return (
+    <>
+      <h1>Homepage</h1>
+      <p>
+        {" "}
+        Go to <Link to="/products"> list of products</Link>.
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
