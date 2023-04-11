ca77702a1b6955ce7779b0ec498bb1bfbb2f1496
yongkheehou <yongkheehou@gmail.com>
2023-04-11 08:28:50 +0800

    using useRouteLoaderData to access data from other routes

--- advanced_routing/frontend/src/App.js
+++ advanced_routing/frontend/src/App.js
@@ -23,7 +23,9 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import EditEventPage from "./pages/EditEvent";
-import EventDetailPage from "./pages/EventDetail";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
@@ -47,9 +49,19 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: ":eventId", element: <EventDetailPage /> },
+          {
+            path: ":eventId",
+            id: "event-detail",
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+              },
+              { path: "edit", element: <EditEventPage /> },
+            ],
+          },
           { path: "new", element: <NewEventPage /> },
-          { path: ":eventId/edit", element: <EditEventPage /> },
         ],
       },
     ],
--- advanced_routing/frontend/src/components/EventsList.js
+++ advanced_routing/frontend/src/components/EventsList.js
@@ -1,4 +1,6 @@
-import classes from './EventsList.module.css';
+import classes from "./EventsList.module.css";
+
+import { Link } from "react-router-dom";
 
 function EventsList({ events }) {
   return (
@@ -7,13 +9,13 @@ function EventsList({ events }) {
       <ul className={classes.list}>
         {events.map((event) => (
           <li key={event.id} className={classes.item}>
-            <a href="...">
+            <Link to={event.id}>
               <img src={event.image} alt={event.title} />
               <div className={classes.content}>
                 <h2>{event.title}</h2>
                 <time>{event.date}</time>
               </div>
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
