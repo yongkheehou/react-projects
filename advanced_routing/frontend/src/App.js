356df2eec781bd71440bbdf3e3bd25d25437406e
yongkheehou <yongkheehou@gmail.com>
2023-04-12 08:05:18 +0800

    action() functions

--- advanced_routing/frontend/src/App.js
+++ advanced_routing/frontend/src/App.js
@@ -29,7 +29,7 @@ import EventDetailPage, {
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
 
@@ -61,7 +61,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
