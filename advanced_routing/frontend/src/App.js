89254e3c6fa3ad3f0c15ae5c7ecf220763717eeb
yongkheehou <yongkheehou@gmail.com>
2023-04-12 08:17:35 +0800

    deleting data

--- advanced_routing/frontend/src/App.js
+++ advanced_routing/frontend/src/App.js
@@ -25,6 +25,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import EditEventPage from "./pages/EditEvent";
 import EventDetailPage, {
   loader as eventDetailLoader,
+  action as deleteEventAction,
 } from "./pages/EventDetail";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -57,6 +58,7 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
               { path: "edit", element: <EditEventPage /> },
             ],
