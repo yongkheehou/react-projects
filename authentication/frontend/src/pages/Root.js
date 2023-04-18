045bbcb390eb2828d0e01940c43222938ea6a79f
yongkheehou <yongkheehou@gmail.com>
2023-04-18 09:43:58 +0800

    logging user out

--- authentication/frontend/src/pages/Root.js
+++ authentication/frontend/src/pages/Root.js
@@ -1,9 +1,28 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {
+  Outlet,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    // clears token after 1 hr
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
 
   return (
     <>
