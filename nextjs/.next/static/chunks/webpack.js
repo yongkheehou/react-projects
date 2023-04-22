afae73e1a7f1bb4ed9db8c614a4338ad2e464031
yongkheehou <yongkheehou@gmail.com>
2023-04-22 23:50:11 +0800

    run next app

--- nextjs/.next/static/chunks/webpack.js
+++ nextjs/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e9ba86bc249e4a31"; }
+/******/ 		__webpack_require__.h = function() { return "6c6de7c130ba0a87"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
