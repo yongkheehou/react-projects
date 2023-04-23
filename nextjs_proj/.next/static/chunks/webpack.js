d76b362b760a2a093de1feffa5b484470143ef51
yongkheehou <yongkheehou@gmail.com>
2023-04-23 10:39:59 +0800

    app layout wrapper in _app.js

--- nextjs_proj/.next/static/chunks/webpack.js
+++ nextjs_proj/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ce4ad50c01559ef"; }
+/******/ 		__webpack_require__.h = function() { return "525901c07f20daf3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
