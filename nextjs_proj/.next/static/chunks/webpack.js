c5ab283d00741922774f413cb40a1cfc59c586ff
yongkheehou <yongkheehou@gmail.com>
2023-04-23 10:43:06 +0800

    using programmatic (imperative) navigation

--- nextjs_proj/.next/static/chunks/webpack.js
+++ nextjs_proj/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "525901c07f20daf3"; }
+/******/ 		__webpack_require__.h = function() { return "8a0c721882d0ea5b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
