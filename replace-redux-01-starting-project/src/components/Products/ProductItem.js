799c89724cfdaeaa1a26aacadd5a106763aa7a13
yongkheehou <yongkheehou@gmail.com>
2023-05-02 08:40:48 +0800

    toggling favorites with context API

--- replace-redux-01-starting-project/src/components/Products/ProductItem.js
+++ replace-redux-01-starting-project/src/components/Products/ProductItem.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 // import { useDispatch } from 'react-redux';
 
 import Card from "../UI/Card";
 import "./ProductItem.css";
-import { toggleFav } from "../../store/actions/products";
+// import { toggleFav } from "../../store/actions/products";
+import { ProductsContext } from "../../context/products-context";
 
 const ProductItem = (props) => {
+  const toggleFav = useContext(ProductsContext).toggleFav;
   // const dispatch = useDispatch();
 
   const toggleFavHandler = () => {
     // dispatch(toggleFav(props.id));
+    toggleFav(props.id);
   };
 
   return (
