287f2e0fa25f3c7acb3676fc94e7f316b543c391
yongkheehou <yongkheehou@gmail.com>
2023-05-03 09:02:43 +0800

    custom store as a hook

---
+++ replace-redux-01-starting-project/src/hooks-store/store.js
@@ -0,0 +1,18 @@
+// managing state globally with react and javascript
+import { useEffect, useState } from "react";
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+const useStore = () => {
+  const setState = useState(globalState)[1];
+
+  useEffect(() => {
+    listeners.push(setState);
+
+    return () => {
+      listeners = listeners.filter((li) => li != setState);
+    };
+  }, [setState]);
+};
