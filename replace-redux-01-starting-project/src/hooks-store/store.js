395f9c4205fdd6d57f7eb5d481551c12160e142d
yongkheehou <yongkheehou@gmail.com>
2023-05-03 09:13:28 +0800

    complete store hook

--- replace-redux-01-starting-project/src/hooks-store/store.js
+++ replace-redux-01-starting-project/src/hooks-store/store.js
@@ -5,8 +5,17 @@ let globalState = {};
 let listeners = [];
 let actions = {};
 
-const useStore = () => {
+export const useStore = () => {
   const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier) => {
+    const newState = actions[actionIdentifier](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
 
   useEffect(() => {
     listeners.push(setState);
@@ -15,4 +24,14 @@ const useStore = () => {
       listeners = listeners.filter((li) => li != setState);
     };
   }, [setState]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+
+  actions = { ...actions, ...userActions };
 };
