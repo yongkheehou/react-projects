a2c096a977a4927c09d3c2f2d5b8b18f47ace5f3
yongkheehou <yongkheehou@gmail.com>
2023-05-05 10:29:42 +0800

    grouping tests in a suite

--- testing/src/components/Greeting.test.js
+++ testing/src/components/Greeting.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders Hello World as a text", () => {
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
+    // Arrange
+    render(<Greeting />);
 
-  const helloWorldElement = screen.getByText("Hello World!");
-  expect(helloWorldElement).toBeInTheDocument();
+    // Act
+    // ... nothing
+
+    // Assert
+    const helloWorldElement = screen.getByText("Hello World!");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
