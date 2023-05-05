7e9813fc95f8d54ba54ce8b0ea931e50acfcb9fd
yongkheehou <yongkheehou@gmail.com>
2023-05-05 10:25:31 +0800

    writing first test

---
+++ testing/src/components/Greeting.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+test("renders Hello World as a text", () => {
+  render(<Greeting />);
+
+  const helloWorldElement = screen.getByText("Hello World!");
+  expect(helloWorldElement).toBeInTheDocument();
+});
