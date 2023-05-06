4c5d92eaf73227f3d87e628ae2535f21782339da
yongkheehou <yongkheehou@gmail.com>
2023-05-06 12:11:46 +0800

    testing user interaction and state

--- testing/src/components/Greeting.test.js
+++ testing/src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -12,5 +13,31 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders good to see you if button was NOT clicked", () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("good to see you", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders Changed if the button was clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render good to see you if the button was clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.queryByText("Changed!", { exact: false });
+    expect(outputElement).toBeNull();
   });
 });
