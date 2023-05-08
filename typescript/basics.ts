f53d08f52c12cc7e350aba23a9ec5ae1b248c193
yongkheehou <yongkheehou@gmail.com>
2023-05-08 11:06:59 +0800

    advanced typescript types

--- typescript/basics.ts
+++ typescript/basics.ts
@@ -9,3 +9,28 @@ userName = 'Max'
 let isInstructor: boolean;
 
 isInstructor = true;
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking']
+
+let person: {
+    name: string, 
+    age: number
+    // isEmployee: boolean;
+}
+
+person = {
+    name: 'Max',
+    age: 32
+}
+
+// person = {
+//     isEmployee: true
+// }
+
+let people: {
+    name: string, 
+    age: number
+    // isEmployee: boolean;
+}[]
