f79daad7cc2033ef3657bc63a93b6042a99047ee
yongkheehou <yongkheehou@gmail.com>
2023-05-09 10:26:08 +0800

    todos function

---
+++ typescript_proj/src/components/Todos.tsx
@@ -0,0 +1,12 @@
+function Todos() {
+    return <ul>
+        <li>
+            Learn React
+        </li>
+        <li>
+            Learn TypeScript
+        </li>
+    </ul>
+}
+
+export default Todos;
