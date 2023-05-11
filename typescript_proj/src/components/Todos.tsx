c53e33ea2ed53888dbf5c123c3f80ab4cca8ae34
yongkheehou <yongkheehou@gmail.com>
2023-05-11 08:40:09 +0800

    outsourcing todoitem list to another component

--- typescript_proj/src/components/Todos.tsx
+++ typescript_proj/src/components/Todos.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Todo from "../models/todos";
-
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{items: Todo[]}> = props => {
     return <ul>
-        {props.items.map(item => <li key={item.id}>{item.text}</li>)}
+        {props.items.map(item => (<TodoItem key={item.id} text={item.text}/>))}
     </ul>
 }
 
