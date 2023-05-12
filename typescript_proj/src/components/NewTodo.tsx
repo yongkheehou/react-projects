5ffcf8f6ea74a6526a113223e6a900b5984a1457
yongkheehou <yongkheehou@gmail.com>
2023-05-12 10:26:38 +0800

    adding styling

--- typescript_proj/src/components/NewTodo.tsx
+++ typescript_proj/src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import classes from './NewTodo.module.css'
 
 
 const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
@@ -15,7 +16,7 @@ const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
         props.onAddTodo(enteredText);
     }
 
-    return (<form onSubmit={submitHandler}>
+    return (<form onSubmit={submitHandler} className={classes.form}>
         <label htmlFor="text">Todo text</label>
         <input type='text' id='text' ref={todoTextInputRef}/>
         <button>Add Todo</button>
--- typescript_proj/src/components/Todos.tsx
+++ typescript_proj/src/components/Todos.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Todo from "../models/todos";
 import TodoItem from "./TodoItem";
+import classes from './Todos.module.css'
 
 const Todos: React.FC<{items: Todo[]}> = props => {
-    return <ul>
+    return <ul className={classes.todos}>
         {props.items.map(item => (<TodoItem key={item.id} text={item.text}/>))}
     </ul>
 }
